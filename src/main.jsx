e1559c416683a686032b7b9d800b6c511b0999b0
moonjihyeon1994 <puppy613@hanmail.net>
2022-04-14 14:00:36 +0900

    add router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,47 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from "./App";
+import "./index.css";
+/*
+예시 입니다. 이런식으로 가져다와서 쓰시죠.
+import Acthistory from './components/acthistory.ks'
+import Auction from './pages/List'
+import Chatting from './pages/Create'
+import Community from './pages/Update'
+import Payment from './pages/Delete'
+import Sale from './pages/Delete'
+import User from './pages/Delete'
+*/
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        {/* HOME은 나중에 */}
+        <Route path="/" element={<App />} />
+
+        {/* acthistory */}
+        {/* <Route path="/acthistory" element={<Acthistory />} /> */}
+
+        {/* auction */}
+        {/* <Route path="/auction" element={<Auction />} /> */}
+
+        {/* chatting */}
+        {/* <Route path="/chatting" element={<Chatting />} /> */}
+
+        {/* community */}
+        {/* <Route path="/community" element={<Community />} /> */}
+
+        {/* payment */}
+        {/* <Route path="/payment" element={<Payment />} /> */}
+
+        {/* sale */}
+        {/* <Route path="/sale" element={<Sale />} /> */}
+
+        {/* user */}
+        {/* <Route path="/user" element={<User />} /> */}
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
-)
+);
