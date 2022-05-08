48bf0291e4bfce7466618c20d10a3b950afa74f7
Ted <skcchwang@gmail.com>
2022-05-08 15:50:35 +0900

    second

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,10 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
 import "./index.css";
+import AuctionList from './pages/auction/AuctionList';
+import Regist from './pages/auction/AuctionRegist';
+import Detail from './pages/auction/AuctionDetail';
+import "bootstrap/dist/css/bootstrap.min.css";
 /*
 예시 입니다. 이런식으로 가져다와서 쓰시죠.
 import Acthistory from './components/acthistory.ks'
@@ -26,6 +30,9 @@ ReactDOM.createRoot(document.getElementById("root")).render(
 
         {/* auction */}
         {/* <Route path="/auction" element={<Auction />} /> */}
+        <Route path="/auction/auctions" element={<AuctionList />}></Route>
+        <Route path="/auction/regists" element={<Regist />}></Route>
+        <Route path="/auction/details" element={<Detail />}></Route>
 
         {/* chatting */}
         {/* <Route path="/chatting" element={<Chatting />} /> */}
